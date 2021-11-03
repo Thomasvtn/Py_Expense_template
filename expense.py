a10dc315c3dfa2c92ef21cd211dcbfd96cf3a787
Thomasvtn <vaton-thomas@outlook.fr>
2021-11-03 21:34:05 +0100

    Add obligation of choosing spender in the list

--- expense.py
+++ expense.py
@@ -1,5 +1,24 @@
+from os import name, read
 from PyInquirer import prompt
 import csv
+
+from prompt_toolkit.shortcuts import print_tokens
+
+
+def spender_list(answers):
+    f = open('csv/users.csv')
+    reader = csv.reader(f)
+    name_list = list(reader)
+
+    f.close()
+
+    res = []
+
+    for row in name_list:
+        res.append(row[0])
+
+    return res
+
 
 expense_questions = [
     {
@@ -13,10 +32,11 @@ expense_questions = [
         "message": "New Expense - Label: ",
     },
     {
-        "type": "input",
+        "type": "list",
         "name": "spender",
         "message": "New Expense - Spender: ",
-    },
+        "choices": spender_list
+    }
 
 ]
 
@@ -26,7 +46,9 @@ def new_expense(*args):
     writer = csv.writer(f)
 
     infos = prompt(expense_questions)
+
     writer.writerow([infos['amount'], infos['label'], infos['spender']])
+
     print("Expense Added !")
 
     f.close()
